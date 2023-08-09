cf4dd06e24875ac0a57a2a159ea0e8c22d7adb7f
luinor223 <vile2040@gmail.com>
2023-08-09 15:05:40 +0700

    Done GET PUSH HEAD, exception

--- ProxyServer.py
+++ ProxyServer.py
@@ -2,36 +2,71 @@ import socket
 import sys
 import threading
 import time as pytime
-import configparser
+from configparser import ConfigParser
 from datetime import datetime, time 
-
-def send_response(client_socket, status_code, content_type, content):
-    header = f"HTTP/1.1 {status_code}\r\n"
-    header += f"Content-Length: {len(content)}\r\n"
-    header += f"Content-Type: {content_type}\r\n\r\n"
-    response = header.encode("utf8") + content.encode("utf8")
-    client_socket.send(response)
     
-def GET(client_socket, rq_url):
-    if rq_url == "":
-        url = "index.html"
-        content_type = "text/html"
-    if rq_url = "favicon.ico"
-    
-    
-
-def handle_client(client_socket, client_address):
-    request = client_socket.recv(4096).decode("utf-8")
+def process_request(request):
     first_line = request.split("\r\n")[0]
     method = first_line.split(" ")[0]
     url = first_line.split(" ")[1]
-    url = url.strip("/")
+    http_pos = url.find("://") # find pos of ://
+    if (http_pos==-1):
+        temp = url
+    else:
+        temp = url[(http_pos+3):] # get the rest of url
+
+    port_pos = temp.find(":") # find the port pos (if any)
+
+    # find end of web server
+    webserver_pos = temp.find("/")
+    if webserver_pos == -1:
+        webserver_pos = len(temp)
+
+    webserver = ""
+    port = -1
+    if (port_pos==-1 or webserver_pos < port_pos): 
+        # default port 
+        port = 80 
+        webserver = temp[:webserver_pos] 
+
+    else: # specific port 
+        port = int((temp[(port_pos+1):])[:webserver_pos-port_pos-1])
+        webserver = temp[:port_pos]
     
+    return method, url, webserver, port
+
+def send_403_response(client_socket):
+    header = "HTTP/1.0 403 Forbidden\r\n"
+    header += "Content-Type: text/html\r\n\r\n"
+    with open("403.html", 'r') as file:
+        content = file.read()
+    client_socket.send(header.encode() + content.encode())
+    return
+    
+def handle_client(client_socket, client_address):
+    #Receive request from Client
+    request = client_socket.recv(4096).decode()
+    method, url, webserver, port = process_request(request)
+    
+    #Check method
+    if method not in ["GET", "POST", "HEAD"]:
+        send_403_response(client_socket)
+        return
+    
+    #Request to webserver
     print(f"Request from {client_address} : {method} {url}")
-    #client_socket.sendall(b"HTTP/1.1 200 OK\r\n")
+    ProxyClientSock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ProxyClientSock.connect((webserver, port))
+    ProxyClientSock.send(request.encode())
+    while 1:
+        #Receive response from webserver
+        message = ProxyClientSock.recv(4096)
+        #Send response to client
+        client_socket.send(message)
+        if len(message) <= 0:
+            break
     
-    #if method == "GET":
-        #GET(client_socket, url)
+    client_socket.close()
         
 
 def main():
@@ -51,7 +86,8 @@ def main():
 
     while True:
         tcpCliSock, addr = tcpSerSock.accept()
-        handle_client(tcpCliSock, addr)
+        thread = threading.Thread(target=handle_client, args=(tcpCliSock, addr))
+        thread.start()
     
 if __name__ == "__main__":
     main()
